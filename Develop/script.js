4ee0d35cd858533371f716d70ef532f17e3499aa
mdpais <mdpais@gmail.com>
2022-11-23 21:22:34 -0400

    Added date in header

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,15 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
+$( document ).ready( readyFn );
+
+function readyFn() {
+
+  $('#currentDay').text(dayjs().format('dddd, MMMM D YYYY'));
+  // var date = document.querySelector("#currentday");
+  // date.textContent = dayjs().format('dddd, MMMM D YYYY');
+
 $(function () {
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +28,5 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+}); 
+}
